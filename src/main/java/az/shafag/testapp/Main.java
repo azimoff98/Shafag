87ab83d0dbba02c808dff3534be50526bc5dfffa
Azimli Gurban Elnur <azim_ns74@ITSTEP.LOCAL>
2018-09-08 16:54:29 +0400

    second commit

---
+++ src/main/java/az/shafag/testapp/Main.java
@@ -0,0 +1,12 @@
+package az.shafag.testapp;
+
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+}
