e14047d3ea3e82265edc752fcbbaf5766c7f65f9
Hamzatli Ismayil Khamis <Hamz_aj04@ITSTEP.LOCAL>
2018-10-21 16:06:44 +0400

    Add advertsiment done

---
+++ static/src/main/resources/static/javascripts/ElanYerlestir.js
@@ -0,0 +1,13 @@
+
+
+function currency(){
+
+
+
+    if(document.getElementById("id").checked){
+
+        document.getElementById("inputGroupFile10").innerHTML = ' <input placeholder="USD"  type="text" class="form-control text-center"\n' +
+            '                       aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm">'
+    }
+
+}
